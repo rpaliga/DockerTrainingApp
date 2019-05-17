607148b301fd69b65f4919655c4cfd57b3add69a
Damian <kontakt@dszymczuk.pl>
2019-05-17 23:34:15 +0200

    create api server

---
+++ api_server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todoapp'
+});
+
+app.use(cors());
+
+
+app.get('/api/tasks', (req, res) => {
+  // connection.connect();
+
+  connection.query('select * from tasks', function(error, results, fields) {
+    if (error) throw error;
+
+
+    return res.json(results);
+  });
+
+  // connection.end();
+});
+
+app.listen(5000, () => console.log('Api server listening on port 5000!'));
